b3e5aae6b5a3864f83d69b8101c8dd1d9c201e95
laszekQ <jalexeyvlyakhovichj@protonmail.com>
2026-04-24 13:03:20 +0200

    CountryManager base

---
+++ Assets/Scripts/Countries/CountryManager.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountryManager : MonoBehaviour
+{
+    public Dictionary<string, Country> Countries { get; private set; }
+    
+    public static CountryManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if(instance != this) 
+            Destroy(this);
+        
+        DontDestroyOnLoad(this);
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ApplyEffect(string countryId, Effect e)
+    {
+        
+    }
+}
