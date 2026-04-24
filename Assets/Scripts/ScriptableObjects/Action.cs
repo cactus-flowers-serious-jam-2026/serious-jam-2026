f79d33bb50ce93e27a2290d947c21ce81101e5b7
laszekQ <jalexeyvlyakhovichj@protonmail.com>
2026-04-24 12:45:50 +0200

    Action

---
+++ Assets/Scripts/ScriptableObjects/Action.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+[CreateAssetMenu(fileName = "Action", menuName = "Scriptable Objects/Action")]
+public class Action : ScriptableObject
+{
+    public string Description;
+    public Effect Impact;
+    public int Timeout; // -1 - means one-time action, 0 - no timeout, 1+ - timeout in ticks 
+    public Dictionary<Parameter, int> Cost;
+    public bool Active = true;
+    private int timeoutCounter;
+
+    public void OnTickEvent()
+    {
+        if (!Active && Timeout > 0)
+        {
+            timeoutCounter = Timeout + 1;
+            if (timeoutCounter == Timeout)
+                Active = true;
+        }
+    }
+
+    public void Activate()
+    {
+        if(!Active)
+            return;
+        
+        if(Timeout == -1)
+            Active = false;
+        
+        else if (Timeout > 0)
+        {
+            Active = false;
+            timeoutCounter = 0;
+        }
+    }
+}
