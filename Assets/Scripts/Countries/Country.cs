f4fc1518095f91145382936ff96145318e6efc5f
laszekQ <jalexeyvlyakhovichj@protonmail.com>
2026-04-25 11:48:01 +0200

    Regions IDs, Effect changes

--- Assets/Scripts/Countries/Country.cs
+++ Assets/Scripts/Countries/Country.cs
@@ -1,16 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Country : MonoBehaviour
 {
+    [SerializeField]
+    private Region[] _regions;
+    
+    [SerializeField]
+    private int[] regionIDs;
+    
+    private Dictionary<Parameter, float> _parameters;
+    private Dictionary<string, CountryResource> _countryResources;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _regions = FindObjectsByType<Region>(FindObjectsSortMode.None)
+                    .Where(r => regionIDs.Contains(r.ID))
+                    .Select(r => r)
+                    .ToArray();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnResourceGathered(string type, int count)
+    {
+        if (_countryResources.ContainsKey(type))
+            _countryResources[type].IncreaseCount(count);
+    }
+
+    public void ApplyEffect(Effect e)
+    {
+        for (int i = 0; i < e.ParameterChanges[0].Changes.Length; i++)
+        {
+            ParameterChange p = e.ParameterChanges[0].Changes[i];
+            if (_parameters.ContainsKey(p.parameter))
+                _parameters[p.parameter] = Math.Clamp(_parameters[p.parameter] + p.change, 0.0f, 1.0f);
+            
+            Debug.Log(p.parameter + " " + p.change);
+        }
+
+        for (int i = 1; i < e.ParameterChanges.Length; i++)
+            for (int j = 0; j < e.ParameterChanges[i].Changes.Length; j++) 
+                _regions[i].ApplyChange(e.ParameterChanges[i].Changes[j]);
     }
 }
--- Assets/Scripts/Regions/Region.cs
+++ Assets/Scripts/Regions/Region.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Region : MonoBehaviour
 {
+    private static int Counter = 1;
+    public int ID;
+    
     public Dictionary<Parameter, float> Parameters { get; private set; }
     public Action[] Actions { get; private set; }
+
+    void Awake()
+    {
+        ID = Counter++;
+    }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,9 +27,9 @@ public class Region : MonoBehaviour
         
     }
 
-    public void ApplyChange(Effect e)
+    public void ApplyChange(ParameterChange pc)
     {
-        foreach (var pc in e.ParameterChanges)
-            Parameters[pc.parameter] = Math.Clamp(Parameters[pc.parameter] + pc.change, 0.0f, 1.0f);
+        Parameters[pc.parameter] = Math.Clamp(Parameters[pc.parameter] + pc.change, 0.0f, 1.0f);
+        Debug.Log(pc.parameter + " " + pc.change);
     }
 }
