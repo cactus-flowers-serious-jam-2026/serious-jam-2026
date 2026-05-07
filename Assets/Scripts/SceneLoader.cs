0f4268485a93dde8672a81126eb6413178de59b7
laszekQ <jalexeyvlyakhovichj@Protonmail.com>
2026-05-07 16:25:53 +0200

    MainMenuSkeleton

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
